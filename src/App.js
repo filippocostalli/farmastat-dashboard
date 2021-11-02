08abdeb0899ed43dbdc51ef38f1cf4be9588ee5c
fppo.costalli <filppocostalligmail.com>
2021-11-02 12:19:55 +0100

    Create coming soon page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height:
+          "100vh" /* L’unità vh è pari all’1% dell’altezza della viewport (fnestra browser) */,
+        fontFamily: "roboto",
+      }}
+    >
+      <h1
+        style={{
+          color: "#6ec177",
+          margin: 0,
+        }}
+      >
+        Farmastat Dashboard, Come soon
+      </h1>
+      <p>What's upp, coming soon</p>
     </div>
   );
-}
+};
 
 export default App;
